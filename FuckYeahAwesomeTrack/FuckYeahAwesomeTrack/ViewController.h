4fa1d34ae76e5e240d3fb22ac1ca8d012bf437fb
Freenerd <nospam@freenerd.de>
2012-07-07 13:33:57 +0200

    Initial XCode project

---
+++ FuckYeahAwesomeTrack/FuckYeahAwesomeTrack/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FuckYeahAwesomeTrack
+//
+//  Created by johan on 07.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
