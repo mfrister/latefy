3c9891b62eb489b766fcf1eff2b8033df6c3a0fb
Freenerd <nospam@freenerd.de>
2012-07-07 13:31:18 +0200

    No more RubyMotion

--- spec/main_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "Application 'fuck-yeah-awesome-track'" do
-  before do
-    @app = UIApplication.sharedApplication
-  end
-
-  it "has one window" do
-    @app.windows.size.should == 1
-  end
-end
