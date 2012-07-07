3d6819bc3f252fa93363a40b6681e98b7bfdbf4f
Stefan Hintz <github@corbusierhaus.de>
2012-07-07 23:21:44 +0200

    Animation

--- FuckYeahAwesomeTrack/FuckYeahAwesomeTrack/Gracenote.m
+++ FuckYeahAwesomeTrack/FuckYeahAwesomeTrack/Gracenote.m
@@ -87,10 +87,10 @@
 {
 	GNConfig *config = [GNConfig init:GRACENOTE_CLIENT_ID];
 	
-	[config setProperty: @"debugEnabled" value:@"1"];
-	[config setProperty: @"content.musicId.queryPreference.singleBestMatch" value:@"true"];
-	[config setProperty: @"content.coverArt" value:@"true"];
-	[config setProperty: @"content.coverArt.sizePreference" value:@"medium, small"];            
+	[config setProperty:@"debugEnabled" value:@"1"];
+	[config setProperty:@"content.musicId.queryPreference.singleBestMatch" value:@"true"];
+	[config setProperty:@"content.coverArt" value:@"true"];
+	[config setProperty:@"content.coverArt.sizePreference" value:@"medium small"];            
 	
 	return config;
 }
