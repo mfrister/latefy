667d0a7c3fa3a04f957e5c6205a385daeb31c7ca
Stefan Hintz <github@corbusierhaus.de>
2012-07-08 14:18:31 +0200

    RecordingController

---
+++ FuckYeahAwesomeTrack/FuckYeahAwesomeTrack/RecordingController.h
@@ -0,0 +1,13 @@
+//
+//  RecordingController.h
+//  FuckYeahAwesomeTrack
+//
+//  Created by Stefan Hintz on 08.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordingController : UIViewController
+
+@end
